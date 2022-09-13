4f6145b58003031f95bbd42c827a3c2a05d0295f
Abinesh368 <100009522+Abinesh368@users.noreply.github.com>
2022-09-13 11:25:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,8 +9,9 @@ def insertionSort(array) -> List[int]:
     while j>=0 and t<array[j]:
             array [j-1]=array[j]
             j-=1
-       array[j+1]=t
-  return array
+          array[j+1]=t
+          
+          return array
     
     
 
